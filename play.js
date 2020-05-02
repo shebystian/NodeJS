7581ef1ba64725a325334480665bd9cd27822b95
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-05-01 21:16:13 -0400

    video 16/17 Array, objetos y referencias

--- play.js
+++ play.js
@@ -2,19 +2,7 @@
 var age = 33;
 var hasHobbies = true;
 */
-const person = {
-    name: 'Sebastian',
-    age: 33,
-    greet: function() {
-        console.log('Hi, mi name is ' + this.name);
-        /* ó
-         greet() {
-         console.log('Hi, mi name is ' + this.name);
-        */
-    }
-};
 
-person.greet();
 /*
 function summarizeUser(userName, userAge, userHasHobby){
     return (
@@ -28,14 +16,30 @@ function summarizeUser(userName, userAge, userHasHobby){
     );
 }
 */
-
-
-
-
-const add = (a,b) => a+b;
+//const add = (a,b) => a+b;
 //const addOne = a => a + 1:
 //const addRandom = () = 1 + 2;
-
 //console.log(add(1,2));
-
 //console.log(summarizeUser(name, age, hasHobbies));
+
+const person = {
+    name: 'Sebastian',
+    age: 33,
+    greet: function() {
+        console.log('Hi, mi name is ' + this.name);
+        /* ó
+         greet() {
+         console.log('Hi, mi name is ' + this.name);
+        */
+    }
+};
+
+const hobbies = ['Sports', 'Cooking'];
+/* con esto recorro y muestro cada hobby en cada iteracion
+for (let hobby of hobbies){
+    console.log(hobby);
+} 
+*/
+// ahora con .map muestro el array y puedo asignarle un tipo hobby:, el segundo ejemplo hace lo mismo pero solo muestra la data
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+console.log(hobbies)
