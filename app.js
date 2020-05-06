fcc243ace291865da5cde47107a1b0042d192fc7
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-05-05 23:58:51 -0400

    video 30 Envio de Respuestas

--- app.js
+++ app.js
@@ -16,8 +16,15 @@ http.createServer((req,res) => {
 });
 */
 //ahora debo crear una variable constante para almacenar el servidor creado:
-const server = http.createServer((req,res) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    //process.exit(); // con esto detengo el proceso de event loop.
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My first Page </title><head>');
+    res.write('<body><h1> My first Page on NodeJS Server</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
